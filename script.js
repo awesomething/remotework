08fb0a8e23b1bbc1dafd2cf64b95411e6bd5a191
Awesome Henry <henryebomah@gmail.com>
2020-11-30 13:48:15 -0800

    Update script.js

--- script.js
+++ script.js
@@ -63,16 +63,14 @@ function displayVideos(responseJson) {
     //array, add a list item to the results 
     //list with the video title, description,
     //and thumbnail
+    console.log(responseJson);
     $('#videos').append(
       `<li><h3>${responseJson.items[i].snippet.title}</h3>
       <p>${responseJson.items[i].snippet.description}</p>
-    
       <img src='${responseJson.items[i].snippet.thumbnails.default.url}' class="thumbnails">
-      <iframe 
-        width="420" 
-        height="315"
+      <iframe width="420" height="315"
         src="https://www.youtube.com/embed/${responseJson.items[i].id.videoId}">
-      </iframe>
+      </iframe> 
       </li>`
     )};
   //display the results section  
